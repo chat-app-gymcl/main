b41407c24ac1b446f4ad2ae0cc12a304183b0161
VitekDev <vit@vakula.cz>
2021-10-15 19:04:48 +0200

    sprovozňuju notification

--- index.php
+++ index.php
@@ -57,5 +57,25 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
             reloadMessages(); // Ihned aktualizujeme právě odeslanou zprávu
         }
     </script>
+
+    <script>
+        functiom showNotification() {
+            const notification = new Notification("Nová zpráva od <?= $_SESSION["username"] ?? "" ?>", {
+                body: "Uživatel <?= $_SESSION["username"] ?? "" ?> napsal: <?= $_POST["message"] ?? "" ?>"
+            });
+        }
+
+        console.log(Notification.permission);
+
+        if (Notification.permission === "granted") {
+            showNotification();
+        } else if (Notification.permission !== "danied") {
+            Notification.requwstPermission().then(permission => {´
+                if (permission === "granted") {
+                    showNotification();
+                }
+            });
+        }
+    </script>
 </body>
 </html>
