56599db78b687817378eff9b8502145bfe0537e4
VitekDev <vit@vakula.cz>
2021-10-17 09:38:46 +0200

    delam cpatch

--- Auth/index.php
+++ Auth/index.php
@@ -1,23 +1,22 @@
 <?php
-$data = array(
-            'secret' => "my-secret (should start with 0x..)",
-            'response' => $_POST['h-captcha-response']
-        );
-$verify = curl_init();
-curl_setopt($verify, CURLOPT_URL, "https://hcaptcha.com/siteverify");
-curl_setopt($verify, CURLOPT_POST, true);
-curl_setopt($verify, CURLOPT_POSTFIELDS, http_build_query($data));
-curl_setopt($verify, CURLOPT_RETURNTRANSFER, true);
-$response = curl_exec($verify);
-// var_dump($response);
-$responseData = json_decode($response);
-if($responseData->success) {
-    // your success code goes here
-} 
-else {
-   // return error to user; they did not pass
-}
+ 
+  if(isset($_POST['h-captcha-response']) && !empty($_POST['h-captcha-response']))
+  {
+        $secret = 'your_secret_key';
+        $verifyResponse = file_get_contents('https://hcaptcha.com/siteverify?secret='.$secret.'&response='.$_POST['h-captcha-response'].'&remoteip='.$_SERVER['REMOTE_ADDR']);
+        $responseData = json_decode($verifyResponse);
+        if($responseData->success)
+        {
+            $succMsg = 'Your request have submitted successfully.';
+        }
+        else
+        {
+            $errMsg = 'Robot verification failed, please try again.';
+        }
+   }
+?>
 
+<?php
 // Initialize the session
 session_start();
  
