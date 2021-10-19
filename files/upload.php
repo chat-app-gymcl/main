0d4d36aef0d1b6ef8f540e1edfddf136577cdf0b
VitekDev <vit@vakula.cz>
2021-10-19 16:06:05 +0200

    files system

---
+++ files/upload.php
@@ -0,0 +1,32 @@
+<?php
+    if (isset($_POST['submit'])) {
+        $file = $_FILES['file'];
+
+        $fileName = $_FILES['file']['name'];
+        $fileTmpName = $_FILES['file']['tmp_name'];
+        $fileSize = $_FILES['file']['size'];
+        $fileError = $_FILES['file']['error'];
+        $fileType = $_FILES['file']['type'];
+
+        $fileExt = explode('.', $fileName);
+        $fileActualExt = strtolower(end($fileExt));
+
+        $allowed = array('jpg', 'jpeg', 'png', 'pdf');
+
+        if (in_array($fileActualExt, $allowed)) {
+            if ($fileError === 0) {
+                if ($fileSize < 1000000) {
+                    $fileNameNew = uniqid('', true).".".$fileActualExt;
+                    $fileDestination = 'uploads/'.$fileNameNew;
+                    move_uploaded_file($fileTmpName, $fileDestination);
+                    header("Location: index.php?uploadsuccess");
+                } else {
+                    echo "Velikost tvého souboru je moc velká!";
+                }
+            } else {
+                echo "Naskytla se chyba při nahrávání tvého souboru!";
+            }
+        } else {
+            echo "Nemůžeš nahrát soubory tohoto typu!";
+        }
+    }
