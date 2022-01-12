37762304abcc5bf550827af50b8cc67638cd724d
Vitek <90604277+VitekDev@users.noreply.github.com>
2022-01-12 22:23:12 +0100

    Pridan ads do register

--- chat/Auth/register.php
+++ chat/Auth/register.php
@@ -105,6 +105,8 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         body{ font: 14px sans-serif; }
         .wrapper{ width: 360px; padding: 20px; }
     </style>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5706270752195906"
+     crossorigin="anonymous"></script>
 </head>
 <body>
     <div class="wrapper">
