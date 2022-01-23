db69050a47435ac463e4f3d39524a2beae89bdd5
Vitek <vit@vakula.cz>
2022-01-23 21:43:32 +0100

    udelany sub domeny

---
+++ sub/chat/js/privacy.js
@@ -0,0 +1,11 @@
+cookieconsent.run({
+    notice_banner_type: "footline",
+    consent_type: "express",
+    palette: "light",
+    language: "cs",
+    page_load_consent_levels: ["strictly-necessary"],
+    notice_banner_reject_button_hide: true,
+    preferences_center_close_button_hide: false,
+    website_name: "Chat 2AV",
+    website_privacy_policy_url: ".../privacy.html"
+});
