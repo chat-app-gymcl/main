7b231b6e2d173b05c5aca71ee5d82239ff88ba88
Vitek <90604277+VitekDev@users.noreply.github.com>
2023-09-07 19:26:59 +0200

    Delete sub/news directory

--- sub/news/wp-content/themes/startup-blog/sidebar-footer-area.php
+++
@@ -1,8 +0,0 @@
-<?php if ( is_active_sidebar( 'footer-area' ) ) : 
-  $widgets      = get_option( 'sidebars_widgets' );
-	$widget_class = count( $widgets['footer-area'] );
-  ?>
-	<aside id="sidebar-footer-area" class="sidebar widget-area sidebar-footer-area active-<?php echo esc_attr($widget_class); ?>" role="complementary">
-		<?php dynamic_sidebar( 'footer-area' ); ?>
-	</aside>
-<?php endif;
